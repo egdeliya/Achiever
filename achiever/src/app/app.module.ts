f744481e9c2b4b677fa056f69c8005958cdc7c38
egdeliya <egdeliya@mail.ru>
2017-12-05 21:04:26 +0300

    auth v0.1

--- achiever/src/app/app.module.ts
+++ achiever/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+import {HttpModule} from "@angular/http";
+import {firebaseConfig} from "../environments/firebase.config";
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { OtherComponent } from './members/members.component';
+
+import { AuthGuard } from './auth.service';
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    OtherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    routes
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent],
   exports: [AppComponent]
 })
